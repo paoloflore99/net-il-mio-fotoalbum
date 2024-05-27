f13762081f58c5f4bf83f9dcaf7d5c50a3d85663
paolo flore <paoloflore937@gmail.com>
2024-05-27 11:20:05 +0200

    anche entity framework core istallate

---
+++ net-il-mio-fotoalbum/Controllers/FotoWebApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FotoWebApiController : ControllerBase
+    {
+
+    }
+}
